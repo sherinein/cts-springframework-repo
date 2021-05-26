5e93d773238209426743a61b20c5a56456a56cdc
sherine <sherinein@gmail.com>
2021-05-26 07:47:18 +0530

    initial commit

---
+++ eshopping-employee/src/main/java/com/cts/eshopping/App.java
@@ -0,0 +1,13 @@
+package com.cts.eshopping;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
